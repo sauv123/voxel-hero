d67582f7fafbe21051b10f503bb1776796d7fb0c
s <sauveersinha@gmail.com>
2026-07-24 12:30:16 +0200

    Revert home page work section background to cream

--- src/App.jsx
+++ src/App.jsx
@@ -235,14 +235,14 @@ export default function App() {
         }
       });
 
-      // Brutalist Cube -> Work Folder (#0a0a0a)
+      // Brutalist Cube -> Work Folder (#FCFAF2)
       ScrollTrigger.create({
         trigger: '.bc-container',
         start: "bottom 100%",
         end: "bottom 20%",
         scrub: 1.5,
         onUpdate: (self) => {
-          const blendedColor = gsap.utils.interpolate('#050505', '#0a0a0a', self.progress);
+          const blendedColor = gsap.utils.interpolate('#050505', '#FCFAF2', self.progress);
           gsap.set(document.body, { backgroundColor: blendedColor });
         }
       });
